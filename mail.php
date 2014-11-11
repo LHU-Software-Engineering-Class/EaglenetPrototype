c41d1ca54221fa5c9ebf7e9256a03d8a327db617
rshelly <rshelly@lhup.edu>
2014-11-11 13:59:51 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,9 +3,9 @@
 File name: mail.php
 Created by: David Hall
 Created Date: 10/25/2014
-Last Modified by: David Hall
-Last Modified: 10/25/2014
-Version 1.0
+Last Modified by: Robert Shelly
+Last Modified: 11/11/2014
+Version 1.1
 */
 
 //phpMailer auto generated email message
@@ -25,5 +25,8 @@ Version 1.0
 	$mail->AltBody = "Plain text version: You have registered to use the EagleNet discussion forum. 
 	Please copy this number ".$uniq_code." and click here to go here to activate your account http://dev.eaglenet.lhup.edu/verification.php
 	Thank you for using the EagleNet";
-	$mail->Body = "You have registered to use the <b>EagleNet</b> discussion forum.<br/><br/>
-	Please copy this number ".$uniq_code." and click here to <a href='http://dev.eaglenet.lhup.edu/verification.php'> activate your account</a>";
+	$mail->Body = "<img src="EmailLogo.jpg" alt="Eagle Logo"</br></br>
+        You have requested an account on the <b>EagleNet</b>Discussion forum<br/><br/>
+	Please copy this number &nbsp;&nbsp;&nbsp;".$uniq_code."</br> 
+        and click here to <a href='http://dev.eaglenet.lhup.edu/verification.php'> activate your account</a></br>
+        Disregard this email if you did not request an account from EagleNet";
