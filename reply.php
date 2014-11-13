68176141b2c06de65672c0bcd7d34702adfdd326
dkh1058 <dkh1058@lhup.edu>
2014-11-12 20:38:14 -0500

    11/12/14 8:37PM
    diagnosed why replies were not working

--- reply.php
+++ reply.php
@@ -7,6 +7,7 @@ Last Modified: 11/10/2014
 Last Modified by: David Hall
 Version 1.1
 */
+
 include 'connect.php';
 include 'strip_html_tags.php';
 if($_SERVER['REQUEST_METHOD'] != 'POST'){
@@ -14,10 +15,11 @@ if($_SERVER['REQUEST_METHOD'] != 'POST'){
 	echo 'This file cannot be called directly.';
 }
 else{
-	//a real user posted a real reply
 	$post_content = strip_html_tags($_POST['post_content']);
 	$post_title = strip_html_tags($_POST['post_title']);
 	$post_type = strip_html_tags($_POST['post_type']);
+	
+	//a real user posted a real reply
 	$sql = "INSERT 
 		INTO posts
 		(post_content,
@@ -28,9 +30,9 @@ else{
 		post_by
 		)
 		VALUES (
-			'".$post_content."',
-			'".$post_title."',
-			'".$post_type."',
+			'".mysqli_real_escape_string($con,$post_content)."',
+			'".mysqli_real_escape_string($con,$post_title)."',
+			'".mysqli_real_escape_string($con,$post_type)."',
 			NOW(),
 			".mysqli_real_escape_string($con,$_GET['id']).",
 			".$_SESSION['user_id'].")";
