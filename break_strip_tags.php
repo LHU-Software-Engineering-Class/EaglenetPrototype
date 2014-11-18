9376e2cab285434c6e35e062280fd7cfa681e109
Dmuthle2 <Dmuthle2@lhup.edu>
2014-11-18 12:00:58 -0500

    Created break_strip_tags.php
    This file will test the strip tags method
    B/H Revision 1.1 Dan Muthler 11/18/14

---
+++ break_strip_tags.php
@@ -0,0 +1,12 @@
+<?php
+/*
+File Name: Breaking Bad... code.
+Created by: Dan Muthler
+Creation Date: 11/18/14
+Last Modified By: Dan Muthler
+Last Modified Date: 11/18/14
+Desc: The purpose of this code will be to verify that 
+strings passed to the Strip_html_tags.php file are stripped of things that shouldn't be there.
+*/
+
+$
