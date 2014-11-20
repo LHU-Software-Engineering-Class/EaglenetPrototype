a7958dac7b9c21468b8bff21a81db08de983f16e
rshelly <rshelly@lhup.edu>
2014-11-19 23:34:03 -0500

    Updated to at least say something

--- start.php
+++ start.php
@@ -11,6 +11,23 @@ if($_SESSION['signed_in'] == false | $_SESSION['user_status'] != true){
 else {
 include 'header.php';
 include 'sidebar.php';
-echo'<div id="content2">';
+echo'<div id="content2">
+<body bgcolor="darkred">
+<font size="7" color="cyan">
+<p align="center"><b><i>WELCOME TO EAGLENET!</i></b></p>
+</font>
+<font size="4" color="cyan">
+<p align="center"><b><i>99% uptime!</i></b></p>
+</font>
+<dl>
+  <dt><h1><font color="white">TEST!</font></h></dt>
+
+  <ul type ="square">
+  <dd><li><font color="white">still working on the posts</font></li>
+      <li><font color="white">stuff</font></li>
+  </ul>
+  </dd>
+</dl>
+</body>';
 include 'footer.php';
 }
