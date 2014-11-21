f72d5a74cb6b297cf37693d4bf0780913d689f89
Dmuthle2 <Dmuthle2@lhup.edu>
2014-11-20 22:36:45 -0500

    Adding output
    testing various types of strings passed through to the tag stripper

--- break_strip_tags.php
+++ break_strip_tags.php
@@ -9,5 +9,39 @@ Desc: The purpose of this code will be to verify that
 strings passed to the Strip_html_tags.php file are stripped of things that shouldn't be there.
 */
 include 'strip_html_tags.php';
-$foo = strip_html_tags($foo);
-echo $foo;
+include 'header.php';
+
+$links = '<a href="http://www.google.com">Google</a>';
+echo strip_html_tags($links);
+
+$urls = '<img src="https://lh3.googleusercontent.com/-oXbyCuM-vws/AAAAAAAAAAI/AAAAAAAAAAA/c4wTlG8-Ht0/photo.jpg"  width="90" height="90" alt="Users Avatar" border="0" />';
+echo strip_html_tags($urls);
+
+$head = '<head>ENCLOSED DATA</head>';
+echo strip_html_tags($head);
+
+$style = '<style> ENCLOSED DATA...</style>';
+echo strip_html_tags($style);
+
+$script = '<script> ENCLOSED DATA...</script>';
+echo strip_html_tags($script);
+
+$object = '<object> ENCLOSED DATA...</object>';
+echo strip_html_tags($object);
+
+$embed = '<embed> ENCLOSED DATA...</embed>';
+echo strip_html_tags($embed);
+
+$applet = '<applet> ENCLOSED DATA...</applet>';
+echo strip_html_tags($applet);
+
+$noframes = '<noframes> ENCLOSED DATA...</noframes>';
+echo strip_html_tags($noframes);
+
+$noscript = '<noscript> ENCLOSED DATA...</noscript>';
+echo strip_html_tags($noscript);
+
+$noember = '<noembed> ENCLOSED DATA...</noembed>';
+echo strip_html_tags($noembed);
+
+ 
