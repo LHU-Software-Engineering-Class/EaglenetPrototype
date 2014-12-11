5ad3cdc99f93beb35b8af60a94edd4846f654d43
rshelly <rshelly@lhup.edu>
2014-12-11 14:01:46 -0500

    Update advisor.php

--- advisor.php
+++ advisor.php
@@ -22,14 +22,22 @@ else {
 	echo '<div id ="content">
 	<div class ="kona body">
 	<table border="1" style="width 100%" align="center">
+		
 		<tr>
 			<th>Q: How do I know what time I register for classes?</th>
 			<th>A: You can check this on your myHaven page under student/registration</th>
 		</tr>
 		<tr>
+			<th>Q: How do I get cleared for registration</th>
+			<th>A: Your advisor needs to clear you for your registration time, 
+			some advisors will automatically clear their students, most will 
+			require a scheduled meeting beforehand </th>
+		</tr>
+		<tr>
 			<th>Q: How do I know if I am on track for an on-time Graduation?</th>
 			<th>A: You can ask your advisor for a form of required classes and credits and
-			    You can also check in various places on myHaven.</th>
+			    You can also check in various places on myHaven. Your advisor should also be 
+			    working with you and answer any questions you may have.</th>
 		</tr>
 		<tr>
 			<th>Q: Who make up the staff working with the registrar?</th>
@@ -43,18 +51,18 @@ else {
 			8:00 AM to 4:00 PM</th>
 		</tr>
 		<tr>
-			<th>Q: ?</th>
-			<th>A: For generalquestions, walk-in registrations, high school student registrations, 
+			<th>Q: what is the contact information for the registrar office?</th>
+			<th>A: - For generalquestions, walk-in registrations, high school student registrations, 
 			substitution forms, request to resume studies, transfer credits - 570-484-2006</br>
 			
-			Transcripts, enrollment verifications, major changes, advisor changes, address changes
+			-Transcripts, enrollment verifications, major changes, advisor changes, address changes
 			- 570-484-2124</br>
 			
-			schedule of classes, room assignments (for courses), advisor assignments, Act 48, 
+			-schedule of classes, room assignments (for courses), advisor assignments, Act 48, 
 			registration of internships, independant studies, or graduation - 570-484-2008</br>
 			
-			if you are not sure what your question is or what number to call us this one - 570-484-2006</br>
-			Fax -  570-484-2734
+			-if you are not sure what your question is or what number to call us this one - 570-484-2006</br>
+			-Fax -  570-484-2734
 			and email - registrar@lhup.edu
 			</th>
 		</tr>
